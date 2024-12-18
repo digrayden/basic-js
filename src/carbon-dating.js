a202bacf913ff5fb35ec541c940f8c1ef1c3a046
Diana Grayden <graydendi@mail.ru>
2024-12-18 14:26:18 +0300

    add solution for carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || isNaN(parseFloat(sampleActivity))) { 
+    return false;
+  } 
+  const activity = parseFloat(sampleActivity);
+  if (activity <= 0 || activity > MODERN_ACTIVITY) { 
+    return false;
+  }
+  const age = Math.log(MODERN_ACTIVITY / activity) / (0.693 / HALF_LIFE_PERIOD);
+  return Math.ceil(age);
 }
 
 module.exports = {
